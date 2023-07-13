6f5107e2bccde4926b1d490ad4126f77526055a4
Yalcin Kurt <yalcin.kurt89@gmail.com>
2023-07-13 14:31:04 +0200

    Zwei Zahlen summieren und Ergebnis ausgeben

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,9 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+let num1 = parseInt(prompt("Geben Sie die erste Zahl ein:"))
+let num2 = parseInt(prompt("Geben Sie die zweite Zahl ein:"))
+
+let sum = num1 + num2;
+
+console.log("Die Summe von " + num1 + " und " + num2 + " ist: " + sum);
